87edaf2b54ed425d3191b1e714b0525142c7deca
Badica <vb00232@heron049.eps.surrey.ac.uk>
2019-12-06 18:55:22 +0000

    added the contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#home'
-  #get 'contact', to:'home/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact', to:'home#contact'
+  post 'request_contact', to: 'home#request_contact'
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,18 +1,42 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get home" do
     get root_url
     assert_response :success
 
     assert_select 'title', 'My Website'
     assert_select 'h1', 'Judo C&A Management'
-    assert_select 'p', 'Welcome! Here you can manage the competitions and athletes for Judo'
+    assert_select 'p', "Welcome! Here you can manage the competitions and athletes for Judo"
   end
 
   test "should get contact" do
-    get home_contact_url
+    get contact_url
     assert_response :success
+
+    assert_template layout: 'application'
+    assert_select 'title', 'My Website'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params: {name: "Matthew", email: "matthew@me.com",
+      telephone: "1234567890", message: "Hello"}
+
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
   end
 
 end
