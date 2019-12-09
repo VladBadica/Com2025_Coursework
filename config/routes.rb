24e0433a68ac83963e24a343cdb50cc6a0f3226c
Badica <vb00232@heron137.eps.surrey.ac.uk>
2019-12-09 16:45:55 +0000

    Added a navbar adn tidied

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :athletes
   root 'home#home'
+  get 'athletes', to: 'home#athletes'
   get 'contact', to:'home#contact'
   post 'request_contact', to: 'home#request_contact'
 end
