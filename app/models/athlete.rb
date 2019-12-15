5c5a808f657b678c09632deef44655153a4deef0
Badica <vb00232@heron051.eps.surrey.ac.uk>
2019-12-15 16:57:39 +0000

    added validation for athlete input

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,10 +1,15 @@
 class Athlete < ApplicationRecord
     # all information but the description of an athlete must be present
     # the name needs to be unique(we assume all the athletes have different names)
-    validates :name, presence: true, uniqueness: true
-    validates :sex, presence: true
+    validates :name, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z]{0,30}\z/,
+    message: "Only allows letters, max 30 chars" }
+    validates :sex, presence: true, format: { with: /\A(male|female)\z/,
+    message: "Only male or female" }
     validates :weight, presence: true
-    validates :age, presence: true
+    validates :age, presence: true, format: { with: /\A^[1-9][0-9]?$|^100\z/,
+    message: "Age between 18 and 100" }
+    validates :description, format: { with: /\A[a-zA-Z0-9,.()]+\z/,
+    message: "only allows letters, numbers and ,.()" }
 
     # the association bewtween the models
     has_many :athlete_competitions
